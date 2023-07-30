21b4c23c0cc5f26ccfa5e02cb01df728a422a81f
DeepakFromZeus <manedeep2001@gmail.com>
2023-07-30 21:08:36 +0530

    ip changed

--- src/hoc/axios.js
+++ src/hoc/axios.js
@@ -5,7 +5,7 @@ import { errorToast } from 'src/utils/toast'
 // Next we make an 'instance' of it
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:3100'
-  baseURL: 'http://13.48.136.150:3100'
+  baseURL: 'http://16.171.39.169:3100'
 
 })
 
